723e661381279470cc7a9c955da1210197667382
tegar adityansyah kurniawan <132801289+tegaradit@users.noreply.github.com>
2023-11-25 13:41:07 +0700

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,1 +1,20 @@
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'web-jurusan'
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database:', err);
+        return;
+    }
+    console.log('Connected to the database');
+});
+
+// Export database connection untuk digunakan di seluruh aplikasi
+module.exports = db;
 
